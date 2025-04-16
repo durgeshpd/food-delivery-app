1688482523c987effda762875f4a6bf0d1bdeaa7
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2025-04-16 15:20:50 +0530

    Update 2

--- src/components/__test__/RestaurantCard.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import RestaurantCard from "../RestaurantCard";
-
-
-it("should render RestaurantCard Component with props Data", () => {
-
-    render(<RestaurantCard resData={}/>)
-})
